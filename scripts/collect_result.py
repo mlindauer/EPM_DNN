11022235382e10ed6e8c960d3320c81b3c34d4a7
mlindauer <marius.rks@googlemail.com>
2017-09-04 14:56:17 +0200

    FIX collect_result script

--- scripts/collect_result.py
+++ scripts/collect_result.py
@@ -12,76 +12,87 @@ del(tabulate.LATEX_ESCAPE_RULES[u'^'])
 data = {}
 
 scens = ["SPEAR-SWV","SPEAR-IBM","CPLEX-RCW","CPLEX-REG","CPLEX-CORLAT"]
-models = ["DNN", "RF"]
+models = ["RF", "DNN"]
 
 EVA_BUDGETs = [1,3600]
-WC_BUDGET = 86400 # sec
+WC_BUDGET = 172800 # sec
 RUNS = 3
 
 for scen in scens:
     for model in models:
         for EVA_BUDGET in EVA_BUDGETs:
-            
-            data[scen] = data.get(scen,{})
-            data[scen][model] = data[scen].get(model,{})
-            data[scen][model][EVA_BUDGET] = data[scen][model].get(EVA_BUDGET,{})
+            rep_options = [True] if model=="DNN" else [False]
+            for reg in rep_options:
+                print(scen,model,EVA_BUDGET,reg)
 
-            t = []
-            rmse_I = []
-            rmsle_I = []
-            rmse_II = []
-            rmsle_II = []
-            rmse_III = []
-            rmsle_III = []
-            rmse_IV = []
-            rmsle_IV = []
-            
-            for seed in range(1,RUNS+1):
-                with open("{0}_{1}_{2}_{3}_{4}.log".format(scen, model, EVA_BUDGET,WC_BUDGET, seed)) as fp:
-                    for line in fp:
-                        if line.startswith("Training Time: "):
-                            t.append(int(line.split(":")[1]))
-                        elif line.startswith("RMSE (I)"):
-                            rmse_I.append(float(line.split(":")[1]))
-                        elif line.startswith("RMSLE (I)"):
-                            rmsle_I.append(float(line.split(":")[1]))
-                        elif line.startswith("RMSE (II)"):
-                            rmse_II.append(float(line.split(":")[1]))
-                        elif line.startswith("RMSLE (II)"):
-                            rmsle_II.append(float(line.split(":")[1]))
-                        elif line.startswith("RMSE (III)"):
-                            rmse_III.append(float(line.split(":")[1]))
-                        elif line.startswith("RMSLE (III)"):
-                            rmsle_III.append(float(line.split(":")[1]))
-                        elif line.startswith("RMSE (IV)"):
-                            rmse_IV.append(float(line.split(":")[1]))
-                        elif line.startswith("RMSLE (IV)"):
-                            rmsle_IV.append(float(line.split(":")[1]))
-                    
-            best_run = np.argmin(rmsle_valid)
-                    
-            data[scen][model][EVA_BUDGET]["time"] = t[best_run]
-            data[scen][model][EVA_BUDGET]["RMSE (I)"] = rmse_I[best_run]
-            data[scen][model][EVA_BUDGET]["RMSEL (I)"] = rmsle_I[best_run]
-            data[scen][model][EVA_BUDGET]["RMSE (II)"] = rmse_II[best_run]
-            data[scen][model][EVA_BUDGET]["RMSEL (II)"] = rmsle_II[best_run]
-            data[scen][model][EVA_BUDGET]["RMSE (III)"] = rmse_III[best_run]
-            data[scen][model][EVA_BUDGET]["RMSEL (III)"] = rmsle_III[best_run]
-            data[scen][model][EVA_BUDGET]["RMSE (IV)"] = rmse_IV[best_run]
-            data[scen][model][EVA_BUDGET]["RMSEL (IV)"] = rmsle_IV[best_run]
-           
+                data[scen] = data.get(scen,{})
+                data[scen][model] = data[scen].get(model,{})
+                data[scen][model][EVA_BUDGET] = data[scen][model].get(EVA_BUDGET,{})
+    
+                t = [np.inf]
+                rmse_I = [np.inf]
+                rmsle_I = [np.inf]
+                rmse_II = [np.inf]
+                rmsle_II = [np.inf]
+                rmse_III = [np.inf]
+                rmsle_III = [np.inf]
+                rmse_IV = [np.inf]
+                rmsle_IV = [np.inf]
+    
+                for seed in range(1,RUNS+1):
+                    with open("{0}_{1}_{2}_{3}_{4}_{5}.log".format(scen, model, reg, EVA_BUDGET,WC_BUDGET, seed)) as fp:
+                        for line in fp:
+                            if line.startswith("Training Time: "):
+                                t.append(float(line.split(":")[1]))
+                            elif line.startswith("RMSE (I)"):
+                                rmse_I.append(float(line.split(":")[1]))
+                            elif line.startswith("RMSLE (I)"):
+                                rmsle_I.append(float(line.split(":")[1]))
+                            elif line.startswith("RMSE (II)"):
+                                rmse_II.append(float(line.split(":")[1]))
+                            elif line.startswith("RMSLE (II)"):
+                                rmsle_II.append(float(line.split(":")[1]))
+                            elif line.startswith("RMSE (III)"):
+                                rmse_III.append(float(line.split(":")[1]))
+                            elif line.startswith("RMSLE (III)"):
+                                rmsle_III.append(float(line.split(":")[1]))
+                            elif line.startswith("RMSE (IV)"):
+                                rmse_IV.append(float(line.split(":")[1]))
+                            elif line.startswith("RMSLE (IV)"):
+                                rmsle_IV.append(float(line.split(":")[1]))
+    
+                best_run = np.argmin(rmse_I)
+    
+                data[scen][model][EVA_BUDGET]["time"] = t[best_run]
+                data[scen][model][EVA_BUDGET]["RMSE (I)"] = rmse_I[best_run]
+                data[scen][model][EVA_BUDGET]["RMSEL (I)"] = rmsle_I[best_run]
+                data[scen][model][EVA_BUDGET]["RMSE (II)"] = rmse_II[best_run]
+                data[scen][model][EVA_BUDGET]["RMSEL (II)"] = rmsle_II[best_run]
+                data[scen][model][EVA_BUDGET]["RMSE (III)"] = rmse_III[best_run]
+                data[scen][model][EVA_BUDGET]["RMSEL (III)"] = rmsle_III[best_run]
+                data[scen][model][EVA_BUDGET]["RMSE (IV)"] = rmse_IV[best_run]
+                data[scen][model][EVA_BUDGET]["RMSEL (IV)"] = rmsle_IV[best_run]
+
 for budget in EVA_BUDGETs:
-    table_data = [["","","\multicolumn{2}{c}{$\conf_{\text{train}}$","\multicolumn{2}{c}{$\conf_{\text{train}}$"],
+    table_data = [["","","\multicolumn{2}{c}{$\conf_{\\text{train}}$}","\multicolumn{2}{c}{$\conf_{\\text{test}}$}"],
                   ["Domain", "Instances","RF","DNN","RF","DNN"]
                   ]
-    
+
     for scen in scens:
-        rows = [[scen, "$\insts_{\text{train}}$", data[scen]["RF"][EVA_BUDGET]["RMSE (I)"], data[scen]["DNN"][EVA_BUDGET]["RMSE (I)"], data[scen]["RF"][EVA_BUDGET]["RMSE (III)"], data[scen]["DNN"][EVA_BUDGET]["RMSE (III)"]],
-                ["", "$\insts_{\text{train}}$", data[scen]["RF"][EVA_BUDGET]["RMSE (II)"], data[scen]["DNN"][EVA_BUDGET]["RMSE (II)"], data[scen]["RF"][EVA_BUDGET]["RMSE (IV)"], data[scen]["DNN"][EVA_BUDGET]["RMSE (IV)"]]
-                ]
-        table_data.extend(rows)
-        
+        row = [scen, "$\insts_{\\text{train}}$"]
+        for quad in ["I","III"]:
+            for model in models:
+                row.append("$%.2f$" %(data[scen][model][budget]["RMSEL (%s)" %(quad)]))
+        table_data.append(row)
+                
+        row = [scen, "$\insts_{\\text{test}}$"]
+        for quad in ["II","IV"]:
+            for model in models:
+                row.append("$%.2f$" %(data[scen][model][budget]["RMSEL (%s)" %(quad)]))
+        table_data.append(row)
+
     print(tabulate.tabulate(tabular_data=table_data, tablefmt="latex_booktabs"))
+
             
     
     
